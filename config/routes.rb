ec8602cdd19b2170838264614c8e8d464a2b3e38
Rajeev Amatya <rajeevamatya@gmail.com>
2016-06-01 20:08:27 +0545

    working final draft

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+  root to: "books#index"
+
   resources :books do 
   	resources :notes , only: [:create, :destroy]
   end
-  root to: "books#index"
+
+  resources :sessions, only: [:new, :create, :destroy]
+
+  get "/login" => "sessions#new", as: "login"
+  delete "/logout" => "sessions#destroy", as: "logout"
+  
 end
+
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+Reviewer.destroy_all
 Book.destroy_all
 
 Book.create! [
@@ -8,9 +9,21 @@ Book.create! [
 {name: "The Ruby Programming Language", author: "David Flanagan"}
 ]
 
+100.times {|index| Book.create! name: "Book#{index}", author: "Author#{index}"}
+
 
 eloquent = Book.find_by name: "Eloquent Ruby"
 eloquent.notes.create! [
 {title: "Wow", note: "Great book to learn Ruby"},
 {title: "Funny", note: "Doesn't put your to sleep"}
 ]
+
+reviewers = Reviewer.create! [
+{name: "Joe", password: "abc123"},
+{name: "Jim", password: "123abc"}
+]
+
+Book.all.each do |book|
+	book.reviewer = reviewers.sample
+	book.save!
+end
